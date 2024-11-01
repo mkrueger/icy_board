bd050a33096f063e4c8afc31a9f882d4a251bb71
Mike Krüger <mkrueger@posteo.de>
2024-11-01 06:46:24 +0100

    Add file_id.diz generation.

--- tools/prep_diz.py
+++ tools/prep_diz.py
@@ -1,12 +1,12 @@
 import sys
 import os
 
-if len(sys.argv) != 3:
-    print("need 2 arguments")
+if len(sys.argv) != 2:
+    print("need 1 arguments")
     sys.exit(1)
 
 version=""
-cargo = open(os.path.join("crates", sys.argv[1], "Cargo.toml"), "r")
+cargo = open(os.path.join("Cargo.toml"), "r")
 for line in cargo.readlines():
     if line.startswith("version"):
         m = line.index('"')
@@ -14,13 +14,11 @@ for line in cargo.readlines():
         break
 cargo.close()
 
-file_id = open(os.path.join("crates", sys.argv[1], "build", "file_id.diz"), "r")
+file_id = open(os.path.join("data", "file_id.diz"), "r")
 lines = file_id.readlines()
 file_id.close()
 new_lines = list(map(lambda line: line.replace("#VERSION", version), lines))
 
-f = open(sys.argv[2], "w")
+f = open(sys.argv[1], "w")
 f.writelines(new_lines)
 f.close()
-
-print(version)
