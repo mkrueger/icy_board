57fe41a5d72e7dd1fabb820ac2bb9900b2be4f7a
Mike Krüger <mkrueger@posteo.de>
2024-12-21 17:54:02 +0100

    Fixed ssh connection.

--- crates/icy_net/src/connection/ssh.rs
+++ crates/icy_net/src/connection/ssh.rs
@@ -2,7 +2,7 @@
 use async_trait::async_trait;
 use russh::{client::Msg, *};
 use russh_keys::*;
-use std::{io::ErrorKind, net::TcpStream, sync::Arc, time::Duration};
+use std::{borrow::Cow, io::ErrorKind, net::TcpStream, sync::Arc, time::Duration};
 
 use crate::{telnet::TermCaps, Connection, ConnectionType};
 use tokio::{
@@ -20,54 +20,20 @@ pub struct Credentials {
     pub proxy_command: Option<String>,
 }
 
-const SUPPORTED_CIPHERS: &str = "aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm,aes128-gcm@openssh.com,aes256-gcm,aes256-gcm@openssh.com,aes256-cbc,aes192-cbc,aes128-cbc,blowfish-cbc,3des-cbc,arcfour256,arcfour128,cast128-cbc,arcfour";
-const SUPPORTED_KEY_EXCHANGES: &str = "ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group14-sha1,diffie-hellman-group1-sha1";
-
 impl SSHConnection {
     pub async fn open<A: ToSocketAddrs>(addr: &A, caps: TermCaps, credentials: Credentials) -> crate::Result<Self> {
         let ssh = SshClient::connect(addr, &credentials.user_name, credentials.password).await?;
-        println!("Connected to SSH server   ");
+        println!("SSH connection established");
         let channel = ssh.session.channel_open_session().await?;
         let terminal_type: String = format!("{:?}", caps.terminal).to_lowercase();
+        println!("Terminal type: {}", terminal_type);
         channel
             .request_pty(false, &terminal_type, caps.window_size.0 as u32, caps.window_size.1 as u32, 1, 1, &[])
             .await?;
+        println!("PTY requested");
         channel.request_shell(false).await?;
-
+        println!("SSH connection opened");
         return Ok(Self { client: ssh, channel });
-
-        /*
-
-        for addr in addr.to_socket_addrs()? {
-            let session = Session::new()?;
-            let host = addr.ip().to_string();
-            let mut port = addr.port();
-
-            if port == 0 {
-                port = Self::default_port();
-            }
-            session.set_option(SshOption::Hostname(host))?;
-            session.set_option(SshOption::Port(port))?;
-            session.set_option(SshOption::KeyExchange(SUPPORTED_KEY_EXCHANGES.to_string()))?;
-            session.set_option(SshOption::CiphersCS(SUPPORTED_CIPHERS.to_string()))?;
-            session.set_option(SshOption::CiphersSC(SUPPORTED_CIPHERS.to_string()))?;
-            session.set_option(SshOption::Timeout(Duration::from_millis(5000)))?;
-            session.set_option(SshOption::LogLevel(libssh_rs::LogLevel::Warning))?;
-            session.set_option(SshOption::ProxyCommand(credentials.proxy_command))?;
-            session.connect()?;
-
-            //  :TODO: SECURITY: verify_known_hosts() implemented here -- ie: user must accept & we save somewhere
-            session.userauth_password(Some(credentials.user_name.as_str()), Some(credentials.password.as_str()))?;
-
-            let chan = session.new_channel()?;
-            chan.open_session()?;
-            let terminal_type = format!("{:?}", caps.terminal).to_lowercase();
-            chan.request_pty(terminal_type.as_str(), caps.window_size.0 as u32, caps.window_size.1 as u32)?;
-            chan.request_shell()?;
-            session.set_blocking(false);
-        }
-        Err(NetError::CouldNotConnect.into())
-        */
     }
 
     fn default_port() -> u16 {
@@ -88,9 +54,9 @@ impl Connection for SSHConnection {
     async fn read(&mut self, buf: &mut [u8]) -> crate::Result<usize> {
         match self.channel.make_reader().read(buf).await {
             Ok(size) => {
-                /*    if size == 0 {
+                if size == 0 {
                     return Err(std::io::Error::new(std::io::ErrorKind::UnexpectedEof, "").into());
-                }*/
+                }
                 Ok(size)
             }
             Err(e) => Err(std::io::Error::new(ErrorKind::ConnectionAborted, format!("Connection aborted: {e}")).into()),
@@ -110,14 +76,14 @@ impl Connection for SSHConnection {
 
 struct Client {}
 
-// More SSH event handlers
-// can be defined in this trait
-// In this example, we're only using Channel, so these aren't needed.
 #[async_trait]
 impl client::Handler for Client {
     type Error = russh::Error;
 
-    async fn check_server_key(&mut self, _server_public_key: &ssh_key::PublicKey) -> Result<bool, Self::Error> {
+    async fn check_server_key(
+        &mut self,
+        _server_public_key: &ssh_key::PublicKey,
+    ) -> Result<bool, Self::Error> {
         Ok(true)
     }
 }
@@ -128,21 +94,25 @@ pub struct SshClient {
 
 impl SshClient {
     async fn connect<A: ToSocketAddrs>(addrs: A, user: impl Into<String>, password: impl Into<String>) -> crate::Result<Self> {
-        // let key_pair = load_secret_key(key_path, None)?;
+        let mut preferred = Preferred::DEFAULT.clone();
+        preferred.kex = Cow::Owned(kex::ALL_KEX_ALGORITHMS.iter().map(|k| **k).collect());
+        preferred.cipher = Cow::Owned(cipher::ALL_CIPHERS.iter().map(|k| **k).collect());
         let config = client::Config {
             inactivity_timeout: Some(Duration::from_secs(5)),
+            preferred,
             ..<_>::default()
         };
-
         let config = Arc::new(config);
         let sh = Client {};
-
-        let mut session = client::connect(config, addrs, sh).await?;
+        println!("Connecting to SSH server");
+        let mut session = russh::client::connect(config, addrs, sh).await?;
+        println!("Connected to SSH server");
         let auth_res = session.authenticate_password(user, password).await?;
-
         if !auth_res {
             return Err("Authentication failed".into());
         }
+
+        println!("Authenticated");
 
         Ok(Self { session })
     }
