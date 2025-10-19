08ac2e7afd611ff549d55f1d90100cc69c8a879f
Mike Krüger <mkrueger@posteo.de>
2025-10-19 16:40:58 +0200

    Removed lread.
    It's now in it's own repository.

--- ppe/lread/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for ver in 300 310 320 330 340 400
-do
-	../../target/debug/pplc --runtime $ver
-done
-
--- ppe/lread/run.sh
+++
@@ -1,2 +0,0 @@
-pplc
-icboard --runppe "WLL;;pwrd:WLL;PPE:/home/mkrueger/work/icy_board/ppe/lread/target/icboard/lread.ppe"
